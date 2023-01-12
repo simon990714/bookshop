0654bacd01b5a19ca49143db7cce8791d81a662f
Simon0714 <chenzhy233@163.com>
2023-01-12 22:35:46 +0800

    springboot 集成 redis 的简单实现

---
+++ Demos/RedisDemo/src/test/java/com/czy/bookshop/RedisDemoMainTest.java
@@ -0,0 +1,40 @@
+package com.czy.bookshop;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+
+@SpringBootTest
+public class RedisDemoMainTest {
+
+    @Autowired
+    RedisTemplate<Object,Object> redisTemplate ;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate ;
+
+
+    @Test
+    void testTemplate(){
+
+        String key = "k1";
+        String value = "v1";
+
+        ValueOperations<Object, Object> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(key,value);
+        System.out.println(valueOperations.get(key));
+
+        ValueOperations<String, String> stringValueOperations = stringRedisTemplate.opsForValue();
+        stringValueOperations.set(key,value);
+        System.out.println(stringValueOperations.get(key));
+
+    }
+
+}

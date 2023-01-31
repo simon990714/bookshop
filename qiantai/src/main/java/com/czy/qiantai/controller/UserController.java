dde35d8c68e42108ba0d11a914f6bbd4981a58e3
Simon0714 <chenzhy233@163.com>
2023-01-31 10:44:59 +0800

    登陆页实现验证码

--- qiantai/src/main/java/com/czy/qiantai/controller/UserController.java
+++ qiantai/src/main/java/com/czy/qiantai/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.czy.qiantai.controller;
 
 
+import com.google.code.kaptcha.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * <p>
@@ -16,6 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    Producer producer;
+
+    @RequestMapping("/getKaptchaImage")
+    public void getKaptchaImage(HttpServletResponse response){
+
+        //生成验证码
+        String codeText = producer.createText();
+        BufferedImage codeImage = producer.createImage(codeText);
+
+        //写出
+        response.setContentType("image/png");
+
+        try {
+            ImageIO.write(codeImage,"png",response.getOutputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
 

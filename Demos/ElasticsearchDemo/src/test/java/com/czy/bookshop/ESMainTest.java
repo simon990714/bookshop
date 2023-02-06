45c4bd15abe111473410f58d910ba4b946b6a770
Simon0714 <chenzhy233@163.com>
2023-02-06 15:47:50 +0800

    ESDemo：测试查询和排序

--- Demos/ElasticsearchDemo/src/test/java/com/czy/bookshop/ESMainTest.java
+++ Demos/ElasticsearchDemo/src/test/java/com/czy/bookshop/ESMainTest.java
@@ -1,9 +1,18 @@
 package com.czy.bookshop;
 
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -40,6 +49,60 @@ public class ESMainTest {
         items.add(new Item(3L,"小米12","手机","小米",new BigDecimal(3999.0),"http://localhost:8080/001.png"));
         itemRepository.saveAll(items);
     }
+
+    /**
+     * 普通查找
+     */
+    @Test
+    void normalSearch(){
+        //查询所有
+        itemRepository.findAll(Sort.by(Sort.Direction.DESC,"price").and(Sort.by(Sort.Direction.DESC,"brand"))).forEach(System.out::println);
+    }
+
+
+    /**
+     * 高级查找
+     */
+    @Test
+    void advancedSearch(){
+        //基本查询
+        MatchQueryBuilder queryBuilder = QueryBuilders.matchQuery("title", "小米");//会分词来进行匹配
+        itemRepository.search(queryBuilder).forEach(System.out::println);
+
+        System.out.println("-----------------------------------");
+
+        //自定义查询
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
+        nativeSearchQueryBuilder.withQuery(queryBuilder);
+        Page<Item> page = itemRepository.search(nativeSearchQueryBuilder.build());
+        //打印
+        System.out.println(page.getTotalPages());
+        System.out.println(page.getTotalElements());
+        page.forEach(System.out::println);
+
+        System.out.println("-----------------------------------");
+
+        //分页查询
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("category","手机");//不会进行分词
+        NativeSearchQueryBuilder nativeSearchQueryBuilder2 = new NativeSearchQueryBuilder();
+        nativeSearchQueryBuilder2.withQuery(termQueryBuilder);
+        nativeSearchQueryBuilder2.withPageable(PageRequest.of(0,2));//是从第0页开始的
+        Page<Item> page2 = itemRepository.search(nativeSearchQueryBuilder2.build());
+        //打印
+        System.out.println(page2.getTotalPages());
+        page2.forEach(System.out::println);
+
+
+        //排序
+        nativeSearchQueryBuilder2.withSort(SortBuilders.fieldSort("price").order(SortOrder.DESC));
+        Page<Item> page3 = itemRepository.search(nativeSearchQueryBuilder2.build());
+        //打印
+        System.out.println(page3.getTotalPages());
+        page3.forEach(System.out::println);
+
+    }
+
+
 
 
 }
